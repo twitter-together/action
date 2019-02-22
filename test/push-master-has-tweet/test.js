f6ee3dcd9d1c7bd43209f0f8762bed9c5f058964
Gregor <gregor@martynus.net>
2019-02-21 23:57:31 -0800

    test: push to master with new tweet

---
+++ test/push-master-has-tweet/test.js
@@ -0,0 +1,57 @@
+/**
+ * This test checks the happy path of a commit to the main branch (master)
+ * which includes a new *.tweet file.
+ */
+
+const path = require('path')
+
+const nock = require('nock')
+const tap = require('tap')
+
+// SETUP
+process.env.GITHUB_EVENT_NAME = 'push'
+process.env.GITHUB_TOKEN = 'secret123'
+process.env.GITHUB_EVENT_PATH = require.resolve('./event.json')
+process.env.GITHUB_REF = 'refs/heads/master'
+process.env.GITHUB_WORKSPACE = path.dirname(process.env.GITHUB_EVENT_PATH)
+
+// set other env variables so action-toolkit is happy
+process.env.GITHUB_WORKFLOW = ''
+process.env.GITHUB_ACTION = ''
+process.env.GITHUB_ACTOR = ''
+process.env.GITHUB_REPOSITORY = ''
+process.env.GITHUB_SHA = ''
+
+// MOCK
+nock('https://api.github.com')
+  // get changed files
+  .get('/repos/gr2m/twitter-together/compare/0000000000000000000000000000000000000001...0000000000000000000000000000000000000002')
+  .reply(200, {
+    files: [
+      {
+        status: 'added',
+        filename: 'tweets/hello-world.tweet'
+      }
+    ]
+  })
+
+  // post comment
+  .post('/repos/gr2m/twitter-together/commits/0000000000000000000000000000000000000002/comments', body => {
+    tap.equal(body.body, 'Tweeted:\n\n- https://twitter.com/gr2m/status/0000000000000000001')
+    return true
+  })
+  .reply(201)
+
+nock('https://api.twitter.com')
+  .post('/1.1/statuses/update.json', body => {
+    tap.equal(body.status, 'Hello, world!')
+    return true
+  })
+  .reply(201, {
+    id_str: '0000000000000000001',
+    user: {
+      screen_name: 'gr2m'
+    }
+  })
+
+require('../..')
