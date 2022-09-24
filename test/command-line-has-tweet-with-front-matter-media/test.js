f33cfcceab1834b3803e67ddbe2db3baa58afac5
MattIPv4 <me@mattcowley.co.uk>
2022-09-24 21:28:23 +0100

    Update command line tests with media

--- test/command-line-has-tweet-with-front-matter-media/test.js
+++ test/command-line-has-tweet-with-front-matter-media/test.js
@@ -6,57 +6,78 @@
 const nock = require("nock");
 const tap = require("tap");
 
+// SETUP
+process.env.TWITTER_API_KEY = "key123";
+process.env.TWITTER_API_SECRET_KEY = "keysecret123";
+process.env.TWITTER_ACCESS_TOKEN = "token123";
+process.env.TWITTER_ACCESS_TOKEN_SECRET = "tokensecret123";
+
 // MOCK
 nock("https://api.twitter.com")
-  .post("/1.1/statuses/update.json", (body) => {
-    tap.equal(body.status, "Cuddly :)\n");
-    tap.equal(body.media_ids, "0000000000000000002");
+  .get("/2/users/me")
+  .reply(200, {
+    data: {
+      id: "123",
+      name: "gr2m",
+      username: "gr2m",
+    }
+  })
+
+  .post("/2/tweets", (body) => {
+    tap.equal(body.text, "Cuddly :)");
+    tap.type(body.media, "object");
+    tap.hasProp(body.media, "media_ids");
+    tap.same(body.media.media_ids, ["0000000000000000002"]);
     return true;
   })
   .reply(201, {
-    id_str: "0000000000000000001",
-    user: {
-      screen_name: "gr2m",
-    },
+    data: {
+      id: "0000000000000000001",
+      text: "Cuddly :) https://t.co/abcdeFGHIJ",
+    }
   });
 
 nock("https://upload.twitter.com")
   .post("/1.1/media/upload.json", (body) => {
-    tap.equal(body.command, "INIT");
-    tap.equal(body.total_bytes, "107352");
-    tap.equal(body.media_type, "image/png");
+    tap.match(body, "Content-Disposition: form-data; name=\"command\"\r\n\r\nINIT");
+    tap.match(body, "Content-Disposition: form-data; name=\"total_bytes\"\r\n\r\n107352");
+    tap.match(body, "Content-Disposition: form-data; name=\"media_type\"\r\n\r\nimage/png");
+    return true;
+  })
+  .reply(202, {
+    media_id_string: "0000000000000000002",
+    expires_after_secs: 86400,
+    media_key: "3_0000000000000000002",
+  })
+
+  .post("/1.1/media/upload.json")
+  .reply(204)
+
+  .post("/1.1/media/upload.json", (body) => {
+    tap.match(body, "Content-Disposition: form-data; name=\"command\"\r\n\r\nFINALIZE");
+    tap.match(body, "Content-Disposition: form-data; name=\"media_id\"\r\n\r\n0000000000000000002");
     return true;
   })
   .reply(201, {
     media_id_string: "0000000000000000002",
+    media_key: "3_0000000000000000002",
+    size: 107352,
+    expires_after_secs: 86400,
+    image: {
+      image_type: "image/png",
+      w: 640,
+      h: 360,
+    }
   })
 
-  .post("/1.1/media/upload.json", () => {
-    // TODO: Body just seems to be a string here, not an object.
-    // tap.equal(body.command, "APPEND");
-    // tap.equal(body.media_id, "0000000000000000002");
-    // tap.equal(body.media_type, "");
-    // tap.equal(body.media, "");
-    // tap.equal(body.segment_index, 0);
-    return true;
-  })
-  .reply(201)
-
-  .post("/1.1/media/upload.json", (body) => {
-    tap.equal(body.command, "FINALIZE");
+  .post("/1.1/media/metadata/create.json", (body) => {
     tap.equal(body.media_id, "0000000000000000002");
+    tap.type(body.alt_text, "object");
+    tap.hasProp(body.alt_text, "text");
+    tap.equal(body.alt_text.text, "Blahaj!");
     return true;
   })
-  .reply(201);
-
-// TODO: Support alt text (twitter library does not support JSON payloads)
-//       https://github.com/desmondmorris/node-twitter/issues/347
-// .post("/1.1/media/metadata/create.json", (body) => {
-//   tap.equal(body.media_id, "0000000000000000002");
-//   tap.equal(body.alt_text.text, "Blahaj!");
-//   return true;
-// })
-// .reply(201);
+  .reply(200);
 
 process.chdir(__dirname);
 process.argv = ["node", "lib/index.js", "--file", "tweets/hello-world.tweet"];
